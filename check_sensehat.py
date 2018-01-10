ded7dff3a651d3ed2e7dd4f5ee14ee5db7d96ff7
Andreas Karfusehr <andreas@karfusehr.de>
2018-01-10 22:41:51 +0100

    Add stuff ...
    ... go away!

--- check_sensehat.py
+++ check_sensehat.py
@@ -6,7 +6,7 @@ import argparse
 
 from sense_hat import SenseHat
 
-def check_temp(warn, crit):
+def check_temperature(warn, crit):
 
     # Grab the temperature for the specified adapter by splitting the string
     # into a list and then getting the index number for the adapter name and
@@ -17,51 +17,61 @@ def check_temp(warn, crit):
     sense = SenseHat()
     temp = sense.get_temperature()
     
-    if warn > crit:
-        print "Warning temp must be less than critical temp"
-        sys.exit(3)
-
-    # Do the actual temperature check/comparison.
-    # For each case we return both the code and the current temperature
-    if temp < warn:
-        return 0, temp
-
-    elif temp > warn and temp < crit:
-        return 1, temp
-
-    elif temp >= crit:
-        return 2, temp
-
-    else:
-        return 3, temp
-        
-
-def main():
-    
-    # Parse command line options with argparse
-    cli_parser = argparse.ArgumentParser() 
-    cli_parser.add_argument('-w', metavar='N', type=float, required=True,
-        help='warning temperature')
-    cli_parser.add_argument('-c', metavar='N', type=float, required=True,
-        help='critical temperature')
-    args = cli_parser.parse_args()
-
-    # Get the return-code and temperature from check_temp function
-    return_code, temp = (check_temp(args.w, args.c)) 
+    #temp = 22
     
     # Print temperature and message and then exit with the correct return code
-    if return_code == 0:
+    if temp < warn:
         print ("OK - Temperature is " + str(temp))
         sys.exit(0)
-    elif return_code == 1:
+    elif temp > warn and temp < crit:
         print ("WARNING - Temperature is " + str(temp))
         sys.exit(1)
-    elif return_code == 2:
+    elif temp >= crit:
         print ("CRITICAL - Temperature is " + str(temp))
         sys.exit(2)
     else:
         print ("UNKNOWN - Something went wrong with " + sys.argv[0] + "!")
         sys.exit(3)
+        
+def check_humidity(warn, crit):
+
+    sense = SenseHat()
+    humidity = sense.get_humidity()
+    
+    #humidity = 22
+    
+    # Print temperature and message and then exit with the correct return code
+    if temp < warn:
+        print ("OK - Humidity is %s %%rH" % humidity)
+        sys.exit(0)
+    elif temp > warn and temp < crit:
+        print ("WARNING - Humidity is %s %%rH" % humidity)
+        sys.exit(1)
+    elif temp >= crit:
+        print ("CRITICAL - Humidity is %s %%rH" % humidity)
+        sys.exit(2)
+    else:
+        print ("UNKNOWN - Something went wrong with " + sys.argv[0] + "!")
+        sys.exit(3)
+
+def main():
+    
+    # Parse command line options with argparse
+    cli_parser = argparse.ArgumentParser()
+    cli_parser.add_argument('-m', metavar='N', type=str, required=True,
+        help='check mode') 
+    cli_parser.add_argument('-w', metavar='N', type=float, required=True,
+        help='warning value')
+    cli_parser.add_argument('-c', metavar='N', type=float, required=True,
+        help='critical value')
+    args = cli_parser.parse_args()
+
+    if args.m == "temperature":
+        check_temperature(args.w, args.c)
+    elif args.m == "humidity":
+        check_humidity(args.w, args.c)
+    else:
+        print "UNKNOWN"
         
 if __name__=='__main__':
     main()
